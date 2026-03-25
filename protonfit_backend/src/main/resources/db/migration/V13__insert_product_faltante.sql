10b0e64aab52377d815a9cc745ee5fa85c3b3dd0
DanielleJeanine <danielle.jeanine92@gmail.com>
2026-03-25 13:10:46 -0300

    add:produto faltante

--- protonfit_backend/src/main/resources/db/migration/V13__insert_product_faltante.sql
+++ protonfit_backend/src/main/resources/db/migration/V13__insert_product_faltante.sql
@@ -4,7 +4,7 @@ VALUES
   'Banco Fixo',
   'Estabilidade e versatilidade para diversos exercícios. O Banco Fixo oferece base firme, design ergonômico e alta durabilidade, garantindo segurança e conforto em treinos de força.',
   'BH39',
-  'https://res.cloudinary.com/dhnaj9mdn/image/upload/v1774225576/BH39_okjbfx.jpg,
+  'https://res.cloudinary.com/dhnaj9mdn/image/upload/v1774225576/BH39_okjbfx.jpg',
   (SELECT category_id FROM category WHERE name = 'Linha Omega'),
   TRUE
 )
